337e9427b96e5ad7f4cb18f2d0ec348391d1bd48
mohende <nassiminformaticien@gmail.com>
2024-10-10 02:29:24 -0400

    add responsiveness

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -35,55 +35,63 @@ const SalesPage = () => {
           ))}
         </select>
       </div>
-      <table className="min-w-full border-collapse border border-gray-200">
-        <thead>
-          <tr>
-            <th className="border border-gray-300 p-2">ID</th>
-            <th className="border border-gray-300 p-2">Customer</th>
-            <th className="border border-gray-300 p-2">Number Of Products</th>
-            <th className="border border-gray-300 p-2">Products Name</th>
-            <th className="border border-gray-300 p-2">Total Price</th>
-            <th className="border border-gray-300 p-2">Date</th>
-            <th className="border border-gray-300 p-2">Status</th>
-          </tr>
-        </thead>
-        <tbody>
-          {sales?.map((sale) => (
-            <tr key={sale.id} className="text-center">
-              <td className="border border-gray-300 p-2">{sale.id}</td>
-              <td className="border border-gray-300 p-2">
-                {sale.customerName}
-              </td>
-              <td className="border border-gray-300 p-2">
-                {sale.numberOfProducts}
-              </td>
-              <td className="border border-gray-300 p-2 max-w-[280px] break-all">
-                {sale.productsName.map((name, index) => (
-                  <div key={index}>{name}</div>
-                ))}
-              </td>
-              <td className="border border-gray-300 p-2">{sale.totalPrice}</td>
-              <td className="border border-gray-300 p-2">
-                {new Date(sale.createdAt || "").toLocaleString()}
-              </td>
-              <td className="border border-gray-300 p-2">
-                <span
-                  className={classNames(
-                    `font-semibold text-md text-gray-900 text-opacity-80 px-2 py-1 rounded-md`,
-                    {
-                      "bg-green-300": sale.status === SaleStatus.open,
-                      "bg-yellow-200": sale.status === SaleStatus.closed,
-                      "bg-red-300": sale.status === SaleStatus.cancelled,
-                    }
-                  )}
-                >
-                  {sale.status}
-                </span>
-              </td>
+      <div className="overflow-x-scroll">
+        <table className="min-w-full border-collapse border border-gray-200">
+          <thead>
+            <tr>
+              <th className="border border-gray-300 p-2">ID</th>
+              <th className="border border-gray-300 p-2">Customer</th>
+              <th className="border border-gray-300 p-2">Number Of Products</th>
+              <th className="border border-gray-300 p-2 hidden md:table-cell">
+                Products Name
+              </th>
+              <th className="border border-gray-300 p-2">Total Price</th>
+              <th className="border border-gray-300 p-2">Date</th>
+              <th className="border border-gray-300 p-2 hidden md:table-cell">
+                Status
+              </th>
             </tr>
-          ))}
-        </tbody>
-      </table>
+          </thead>
+          <tbody>
+            {sales?.map((sale) => (
+              <tr key={sale.id} className="text-center">
+                <td className="border border-gray-300 p-2">{sale.id}</td>
+                <td className="border border-gray-300 p-2">
+                  {sale.customerName}
+                </td>
+                <td className="border border-gray-300 p-2">
+                  {sale.numberOfProducts}
+                </td>
+                <td className="border border-gray-300 p-2 max-w-[280px] break-all hidden md:table-cell">
+                  {sale.productsName.map((name, index) => (
+                    <div key={index}>{name}</div>
+                  ))}
+                </td>
+                <td className="border border-gray-300 p-2">
+                  {sale.totalPrice}
+                </td>
+                <td className="border border-gray-300 p-2">
+                  {new Date(sale.createdAt || "").toLocaleString()}
+                </td>
+                <td className="border border-gray-300 p-2 hidden md:table-cell">
+                  <span
+                    className={classNames(
+                      `font-semibold text-md text-gray-900 text-opacity-80 px-2 py-1 rounded-md`,
+                      {
+                        "bg-green-300": sale.status === SaleStatus.open,
+                        "bg-yellow-200": sale.status === SaleStatus.closed,
+                        "bg-red-300": sale.status === SaleStatus.cancelled,
+                      }
+                    )}
+                  >
+                    {sale.status}
+                  </span>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 };
